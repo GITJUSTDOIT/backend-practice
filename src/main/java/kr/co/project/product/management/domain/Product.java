5cd096f4f4db77999e43e28b33d4c02b3bc317dd
GITJUSTDOIT <kimsunwoo141@naver.com>
2026-03-11 02:07:00 +0900

    9장 끝

--- src/main/java/kr/co/project/product/management/domain/Product.java
+++ src/main/java/kr/co/project/product/management/domain/Product.java
@@ -1,6 +1,8 @@
 package kr.co.project.product.management.domain;
 
 
+import java.util.Objects;
+
 public class Product {
     private Long id;
     private String name;
@@ -9,5 +11,28 @@ public class Product {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Boolean sameId(Long id) {
+        return this.id.equals(id);
+    }
+
+
+
+    public Boolean containsName(String name) {
+        return this.name.contains(name);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
---
+++ src/main/java/kr/co/project/product/management/presentation/ProductDto.java
@@ -0,0 +1,43 @@
+package kr.co.project.product.management.presentation;
+
+import kr.co.project.product.management.domain.Product;
+
+import java.util.Objects;
+
+public class ProductDto {
+    private Long id;
+    private String name;
+    private Integer price;
+    private Integer amount;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+
+    public Boolean sameId(Long id) {
+        return this.id.equals(id);
+    }
+
+    public Boolean containsName(String name) {
+        return this.name.contains(name);
+    }
+
+
+}
