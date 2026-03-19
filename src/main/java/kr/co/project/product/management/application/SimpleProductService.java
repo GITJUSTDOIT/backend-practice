dd729f12c431f643592d769ee7b6112e3bd5e06b
GITJUSTDOIT <kimsunwoo1412@gmail.com>
2026-03-19 19:00:09 +0900

    application에  SimpleProductService Dto를 이용하기

--- src/main/java/kr/co/project/product/management/application/SimpleProductService.java
+++ src/main/java/kr/co/project/product/management/application/SimpleProductService.java
@@ -2,6 +2,8 @@ package kr.co.project.product.management.application;
 
 import kr.co.project.product.management.domain.Product;
 import kr.co.project.product.management.infrastructure.ListProductRepository;
+import kr.co.project.product.management.presentation.ProductDto;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,13 +12,25 @@ public class SimpleProductService {
 
     private ListProductRepository listProductRepository;
 
+    private ModelMapper modelMapper;
+
     @Autowired
-    SimpleProductService (ListProductRepository listProductRepository) {
+    SimpleProductService (ListProductRepository listProductRepository, ModelMapper modelMapper) {
         this.listProductRepository = listProductRepository;
+        this.modelMapper = modelMapper;
     }
 
-    public Product add(Product product) {
-        Product saveProduct = listProductRepository.add(product);
-        return saveProduct;
+    public ProductDto add(ProductDto productDto) {
+
+        // ProductDto를 Product로 변환
+        Product product = modelMapper.map(productDto, Product.class);
+
+        // 레포지토리를 호출하는 코드
+        Product savedProduct = listProductRepository.add(product);
+
+        // Product를 ProductDto로 변환하는 코드
+        ProductDto savedProductDto = modelMapper.map(savedProduct, ProductDto.class);
+
+        return savedProductDto;
     }
 }
